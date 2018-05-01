ad6fd0aa9430b8ea2d768784ceb410bf606ce6ef
Mclainpw <mclainpw@vcu.edu>
2018-05-01 19:19:43 -0400

    WIN WIN

--- QuizGame/Multiplayer.swift
+++ QuizGame/Multiplayer.swift
@@ -705,7 +705,7 @@ class Multiplayer: UIViewController ,MCBrowserViewControllerDelegate, MCSessionD
         
         if score > 0
         {
-            timerNotificationLabel.text = "YOU WON!"
+            timerNotificationLabel.text = "YOU WON! You scored \(score!)/\(possibleScore)"
         }
         else
         {
